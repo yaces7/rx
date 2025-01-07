f3c227a89de9e3841df87b60d88c34de40f5eaed
Kurtay <156544814+retonex@users.noreply.github.com>
2025-01-07 23:55:17 +0300

    Update Footer.jsx

--- src/components/Pages/Main/Footer.jsx
+++ src/components/Pages/Main/Footer.jsx
@@ -2,7 +2,7 @@ const Footer = () => {
     return (
         <div>
             <div className='flex justify-center sm:justify-between mx-0 sm:mx-24 py-4 sm:py-4'>
-                <p className='hidden sm:block'>Copyright © 2024 Kurtayx3</p>
+                <p className='hidden sm:block'>Copyright © 2024 YasinCSR</p>
                 <p>Developed with by <span className='text-sky-500 font-semibold'>YC</span></p>
             </div>
             <span className="shadow-[0_0_1000px_40px_#0ea5e9]"></span>
