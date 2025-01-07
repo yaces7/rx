72d616aae0fee750214aa2c3fd3317a3bd721f34
Kurtay <156544814+retonex@users.noreply.github.com>
2025-01-07 23:44:51 +0300

    Update About.jsx

--- src/components/Pages/About.jsx
+++ src/components/Pages/About.jsx
@@ -6,7 +6,7 @@ const About = () => {
     return (
         <div>
             <Helmet>
-                <title>Kurtay - Hakkımda</title>
+                <title>Yasin - Hakkımda</title>
                 <meta name="description" content="Web tabanlı uygulamalar geliştiriyor ve Discord botları ile ilgileniyorum. Portföyüm ve becerilerim için URL lourity adresini ziyaret edin." />
                 <meta property="og:image" content="/kurtay.png" />
                 <meta name="theme-color" content="#0ea5e9" />
@@ -21,10 +21,10 @@ const About = () => {
                 <div className='py-0 px-0 sm:py-8 sm:px-20'>
                     <div className='flex gap-2 py-8 px-20'>
                         <img className='w-12 h-12 rounded-md mt-2 sm:mt-0' src="/kurtay.png" alt="kurtay" />
-                        <span className='font-bold text-2xl text-gray-300 sm:mt-2'>Kurtay kimdir?</span>
+                        <span className='font-bold text-2xl text-gray-300 sm:mt-2'>Yasin kimdir?</span>
                     </div>
                     <div className='p-4 sm:p-1'>
-                        <h2 className='text-lg text-gray-300 font-medium'>Merhaba, ben Kurtay!</h2>
+                        <h2 className='text-lg text-gray-300 font-medium'>Merhaba, ben Yasin!</h2>
                         <p className='text-gray-400 mt-1'>15 yaşında bir Full Stack Developer olarak, 3 yıldır yazılım dünyasına merak salmış, bu alanda kendimi geliştirmeye çalışan biriyim. Batman'da yaşıyorum ve aynı zamanda Youtube platformunda Oyun ve Yazılım ile ilgili videolar paylaşarak topluluğa katkıda bulunmaya çalışıyorum. Tüm bunlarla birlikte, lise eğitimimi sürdürürken yazılımla ilgili lisans eğitimi almayı hedefliyor ve bu alanda daha ileri düzeyde çalışmalar yapmak istiyorum.</p>
                         <h2 className='text-lg text-gray-300 font-medium mt-2'>Yazılım yolculuğum</h2>
                         <p className='text-gray-400 mt-1'>Yazılım yolculuğuma ilk adımlarımı attığımda, web tabanlı geliştirme konusunda bir tutku hissettim. Şu anda favori dillerim arasında React, HTML, CSS ve JavaScript bulunuyor. Bu dilleri kullanarak etkileyici ve kullanıcı dostu web uygulamaları geliştirmek benim için heyecan verici bir deneyim.</p>
